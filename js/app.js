a8fe1af778871fd5efaa7e957e67b839911a7def
ntxtthomas <apm.tthomas@gmail.com>
2015-11-09 12:24:24 -0600

    initial update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+var Fizz = 3/;
+var Buzz = 5/;
+var FizzBuzz = 3/, 5/;
+
+for (var i = 1; i<101; i++) {
+	console.log(i);
+}
+if (i == 3/;) {
+	console.log("Fizz");
+}
+else if (i == 5/;) {
+	console.log("Buzz");
+}
+if (i == 3/ and 5/;) {
+	console.log("Fizz Buzz");
+}
 
 
 
