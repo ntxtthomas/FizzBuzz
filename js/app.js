fe1133123374f92484e23045e0bd084ac4251c46
ntxtthomas <apm.tthomas@gmail.com>
2015-11-17 11:20:47 -0600

    reviewed push

--- js/app.js
+++ js/app.js
@@ -1,59 +1,32 @@
 $(document).ready(function(){
+	
+	var doc='';
 
 //for loop for the numbers 1 through 100
 	for(var i=1; i<=100; i++) {
-		//document.write(i + "<br/>");
-
+		
 //if the number is divisible by 3 AND 5	(no remainder)
 	if (0 === i%3 && 0 === i%5) {
-    	document.write('fizzBuzz' + '<br>');
+    	doc=doc + 'fizzBuzz' + '<br>';
 	} 	
 
 //if the number is divisible by 3 (no remainder)
 	else if(0 === i%3) {
-    	document.write('fizz' + '<br>');
+    	doc=doc + 'fizz' + '<br>';
     } 	
 
 //if the number is divisible by 5 (no remainder)
 	else if(0 === i%5) {
-    	document.write('buzz' + '<br>');
+    	doc=doc + 'buzz' + '<br>';
 	} 	
 
 //or else, just write the number
 	else {
-		document.write(i + '<br>');
+		doc=doc + i + '<br>';
 	}
-}
-});
 
-//issue: the numbers replace my <h1> in the body.. haven't figured out how to have both. 
-
-/*Alternate path: 
-function isDivisibleByY(i,y){
-	if(i%y==0)
-    	return true;
-	else 
-    	return false
 }
-$('document').ready(function(){
-    var t_s1='<span style="color:green">';
-    var s2='</span><br>';
-    var f_s1='<span style="color:red">';
-    var doc='';
-    for (var i=1; i<=100; i++){
-        var res=isDivisibleByY(i,3);
-        console.log ("DEBUG result for i=%o is %o",i,res); 
-        var s='';
-        if(res){
-            s=t_s1;
-        }
-        else 
-            s=f_s1;
-        s=s+i+s2;
-        console.log(s);
-        doc=doc+s;
-    }
-    console.log("DEBUG "+doc)
-	$('#content').html(doc);
+
+$('#content').html(doc);
+
 });
-*/
